bdc2d139291882d335b16cda122a642a44c2ff69
lowergm <92257300+lowergm@users.noreply.github.com>
2025-10-04 08:22:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,6 @@ def postagens():
     postagem = Postagem.select()
     return render_template("index.html", postagem=postagem)
 
-@app.route("/sobre")
-def pagina_sobre():
-    return """
-        <h1>Sobre a rede social</h1>
-        <p>Essa rede social foi criada com o intuito de criar postagens anônimas para se divertir com memes, ou debater opiniões e etc, anonimamente para garantir que não aconteça cancelamento, xingamentos ou maldições ao seu nome</p>
-        <h1>Quem criou?</h1>
-        <p>Esta rede social foi criada por apenas uma pessoa, chamada Henrique, mas também conhecido como Lower, criei essa rede social sozinho, espero que gostem e interajam</p>
-    """
-
 @app.route("/criar", methods=["POST", "GET"])
 def makePost():
     return render_template("post.html")
@@ -39,6 +30,7 @@ def add_post():
     Postagem.create(titulo=f"{titulo}", texto=f"{texto}", topico=f"{topico}")
     return redirect(url_for("postagens"))
 
+"""
 @app.route("/list_topic", methods=["GET", "POST"])
 def list_topic():
     if request.method == "POST":
@@ -46,6 +38,7 @@ def list_topic():
         topicos = Postagem.select().where(Postagem.topico==f"{topicoPesquisa}")
         return render_template("topico.html", topicos=topicos)
     return redirect(url_for("postagens"))
+"""
 
 if __name__ == "__main__":
     db.connect()
